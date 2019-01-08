7898d3c73b479a40f1919e08159577f7a7ebf8f2
Niels Højen <niels@hojen.net>
2019-01-08 16:44:32 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,32 @@
 #!/bin/sh
 
+execConnect="/mdmctl config"
+
+# add api key if specified
+if [[ ${CONFIG_NAME} ]]; then
+  execConnect="${execConnect} -name ${CONFIG_NAME}"
+  execSwitch="mdmctl config switch -name ${CONFIG_NAME}"
+else
+  execConnect="${execConnect} -name production"
+  execSwitch="mdmctl config switch -name production"
+fi
+
+if [[ ${API_KEY} = true ]]; then
+  execConnect="${execConnect} -api-token ${API_KEY}"
+else
+  execConnect="${execConnect} -api-token NONE"
+fi
+
+if [[ ${SERVER_URL} ]]; then
+  execConnect="${execConnect} -server-url ${SERVER_URL}"
+else
+  execConnect="${execConnect} -server-url https://micromdm.example.com"
+fi
+
+echo "Connecting using: $execServe"
+eval $execServe
+
+fi
+
+
 sleep infinity
